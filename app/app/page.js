9399623c0c016e52e5d5ddc4c1865ce558890146
5013-creator <redpuma5013@gmail.com>
2026-04-08 21:36:35 -0500

    Delete app/app/page.js

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Best Neighborhoods to Stay</h1>
-      <p>Find the perfect neighborhood for your next trip.</p>
-    </main>
-  )
-}
